13f6490d80a425747c24d35fbf057320dcdcda80
ThuyTienUet <tien280198@gmail.com>
2018-08-02 12:57:45 +0700

    update

--- controllers/randomColor.js
+++ controllers/randomColor.js
@@ -2,7 +2,7 @@ module.exports = function () {
     var letters = '0123456789ABCDE';
     var color = '#';
     for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 15)];
     }
     return color;
 }
